3f3a4261f34c6b4a8907af4603b4f8b49e0e34f8
Gustav Sörnäs <gustav@sornas.net>
2021-07-01 12:29:01 +0200

    unwrap -> expect

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use std::{env, error::Error};
 type Result<T> = ::std::result::Result<T, Box<dyn Error + Send + Sync>>;
 
 fn main() {
-    let discord_token = env::var("DISCORD_BOT_TOKEN").expect("Set the Discord bot token via env variable DISCORD_TOKEN");
+    let discord_token = env::var("DISCORD_BOT_TOKEN").expect("set the Discord bot token via env-variable DISCORD_BOT_TOKEN");
 
-    let rt = tokio::runtime::Runtime::new().unwrap();
-    rt.block_on(discord::handle(&discord_token));
+    let rt = tokio::runtime::Runtime::new().expect("unable to create async runtime");
+    rt.block_on(discord::handle(&discord_token)).expect("something went wrong in the Discord handler");
 }
